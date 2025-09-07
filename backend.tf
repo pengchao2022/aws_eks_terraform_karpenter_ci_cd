7c5aef8e2cac89daa8a721aaca2ce6a1a8e49a39
pengchao <pengchao.ma2@outlook.com>
2025-09-08 01:03:10 +0800

    updated

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "eks-karpenter/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-table"
+    dynamodb_table = "terraform-lock"
   }
 }
